ecf2806bac67ff6716c33dcbffaf73b130597d37
iampeterbanjo <peterbanjo@gmail.com>
2019-09-29 22:21:40 +0100

    fix build

---
+++ build/src/index.d.ts
@@ -0,0 +1,8 @@
+declare const _default: {
+    register: (server: any, option: any) => Promise<void>;
+    name: string;
+    version: string;
+    dependencies: string[];
+};
+export default _default;
+//# sourceMappingURL=index.d.ts.map
