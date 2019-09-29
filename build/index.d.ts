8f38a79fb1b51d811ea7b38a854d7374fdc330da
iampeterbanjo <peterbanjo@gmail.com>
2019-09-29 22:31:24 +0100

    ignore build folder

--- build/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    register: (server: any, option: any) => Promise<void>;
-    name: string | undefined;
-    version: string | undefined;
-    dependencies: string[];
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
