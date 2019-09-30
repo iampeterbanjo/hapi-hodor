0cf9545b2a200f8d4597a37b78ad593d87dbba3a
iampeterbanjo <peterbanjo@gmail.com>
2019-09-30 05:57:55 +0100

    add new badge reporter

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	collectCoverage: true,
-	coverageReporters: ['json-summary', 'text-summary'],
+	coverageReporters: ['json-summary', 'text-summary', 'lcov'],
 	collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/node_modules/**'],
 	notify: true,
 	notifyMode: 'failure-change',
